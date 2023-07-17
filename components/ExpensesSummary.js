3c68fc1796101e5e88f5b346d3d50c9d3f5e42cf
S00210326 <s00210326@atu.ie>
2023-07-17 11:31:32 +0200

    stack and components set up

---
+++ components/ExpensesSummary.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+function ExpensesSummary() {
+  return (
+    <View>
+      <Text>Last 7 Days</Text>
+      <Text>$117.95</Text>
+    </View>
+  );
+}
+
+export default ExpensesSummary;
---
+++ screens/ManageExpenses.js
@@ -0,0 +1,6 @@
+import { Text } from "react-native";
+function ManageExpense() {
+  return <Text>ManageExpense Screen STORY</Text>;
+}
+
+export default ManageExpense;
