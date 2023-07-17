e04212073cfde1bc413dcce2083cee6ceb4c86fd
S00210326 <s00210326@atu.ie>
2023-07-17 12:17:24 +0200

    displaying dummy data no styling

--- components/ExpensesOutput.js
+++ components/ExpensesOutput.js
@@ -2,11 +2,44 @@ import { View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-function ExpensesOutput({ expenses }) {
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoees",
+    amount: 68.88,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.88,
+    date: new Date("2021-01-22"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2021-9-19"),
+  },
+  {
+    id: "e4",
+    description: "A pair of books",
+    amount: 15.88,
+    date: new Date("2022-02-19"),
+  },
+  {
+    id: "e5",
+    description: "More books",
+    amount: 18.99,
+    date: new Date("2022-02-19"),
+  },
+];
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
-      <ExpensesSummary />
-      <ExpensesList />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 }
